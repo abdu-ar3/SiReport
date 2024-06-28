dac5e16c0306ca31ca3d322e185ef1c8ab3b259d
abdu-ar3 <abdhurohman7@gmail.com>
2024-06-28 10:49:53 +0700

    Fix debug Regist

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        
 
         event(new Registered($user));
 
